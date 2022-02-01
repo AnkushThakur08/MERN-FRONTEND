a464204d938da6a75e6684f509d0bbfb5648ca3d
Ankush Thakur <atpara99@gmail.com>
2022-02-01 08:34:04 +0530

    Connecting Backend to Frontend

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { API } from "../backend";
 
 const Home = () => {
+    
   return (
     <div>
-      <h1>Ankush Thakur</h1>
+      <h1>Ankush Thakur {API}</h1>
     </div>
   );
 };
