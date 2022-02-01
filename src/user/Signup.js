ec17404c6c2ceeb60f44c3424794e52564ec2f5e
Ankush Thakur <atpara99@gmail.com>
2022-02-01 18:20:27 +0530

    Completed Signup Functionality

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -2,7 +2,46 @@ import React, { useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
+// Method Which interate with DB
+import { signup } from "../auth/helper";
+
 const SignUp = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = (name) => (event) => {
+    // We can create Multiple methods for email and password, but with EVENT(higher order Function) and [name] :... we CAN RESUSE THIS
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      })
+      .catch(console.log("Error in Signup"));
+  };
+
   const signupForm = () => {
     return (
       <div className="row">
@@ -10,17 +49,31 @@ const SignUp = () => {
           <form>
             <div className="form-group py-2">
               <label className="text-light">Name</label>
-              <input className="form-control" type="text" />
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+              />
             </div>
 
             <div className="form-group py-2">
               <label className="text-light">Email</label>
-              <input className="form-control" type="text" type="email" />
+              <input
+                className="form-control"
+                onChange={handleChange("email")}
+                type="text"
+                type="email"
+              />
             </div>
 
             <div className="form-group py-2">
               <label className="text-light">Password</label>
-              <input className="form-control" type="text" type="password" />
+              <input
+                className="form-control"
+                onChange={handleChange("password")}
+                type="text"
+                type="password"
+              />
             </div>
 
             <div class="d-grid py-3">
@@ -34,7 +87,6 @@ const SignUp = () => {
     );
   };
 
-  
   return (
     <Base title="Signup Here!!" description="Enter the Deatils to Signup">
       {signupForm()}
