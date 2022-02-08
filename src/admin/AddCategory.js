0d80f845c6f119d235161f08327d6fa2d728f9aa
Ankush Thakur <atpara99@gmail.com>
2022-02-08 11:58:44 +0530

    CreateCategory Functionality Completed

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -1,15 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Base from "../core/Base";
+import { isAuthenticated } from "../auth/helper";
+import { createCategory } from "./helper/adminapicall";
 
 const AddCategory = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const { user, token } = isAuthenticated();
+
+  const goBack = () => {
+    return (
+      <div className="mt-2">
+        <Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">
+          Admin Home
+        </Link>
+      </div>
+    );
+  };
+
+  const handleChange = (event) => {
+    setError("");
+    setName(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setError("");
+    setSuccess(false);
+
+    // Backend Request
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(true);
+      } else {
+        setError("");
+        setSuccess(true);
+        setName("");
+      }
+    });
+  };
+
+  const successMessage = () => {
+    if (success) {
+      return (
+        <p className="display-6 text-success">Category Created Successfully</p>
+      );
+    }
+  };
+
+  const errorMessage = () => {
+    if (error) {
+      return (
+        <p className="display-6 text-warning">Unable to Create The Category</p>
+      );
+    }
+  };
+
+  const myCategoryForm = () => (
+    <form>
+      <div className="form-group">
+        <p className="lead">Enter the Category</p>
+        <input
+          type="text"
+          className="form-control my-3"
+          placeholder="Ex. Summer"
+          onChange={handleChange}
+          value={name}
+          autoFocus
+          required
+        />
+
+        <button onClick={onSubmit} className="btn btn-outline-info mb-2">
+          Create Category
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <Base
       title="Create a Category"
-      description="Add New Category"
-      className="conatiner bg-info p-4"
+      description="Add a New Category for T-Shirts"
+      className="container bg-info p-4"
     >
       <div className="row bg-white rounded">
-        <div className="col-md-8 offset-md-2">Hello Ankush</div>
+        <div className="col-md-8 offset-md-2">
+          {successMessage()}
+          {errorMessage()}
+          {myCategoryForm()}
+          {goBack()}
+        </div>
       </div>
     </Base>
   );
