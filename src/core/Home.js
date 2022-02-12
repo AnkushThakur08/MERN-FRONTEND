f904f86c7cccad2d3ba6b936b23ac6bf7d456baf
Ankush Thakur <atpara99@gmail.com>
2022-02-12 12:12:34 +0530

    Making Backend Call to fetch the Products

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,22 +1,39 @@
-import React from "react";
-import { API } from "../backend";
+import React, { useState, useEffect } from "react";
 import Base from "./Base";
 import Card from "./Card";
 import NavBar from "./NavBar";
 
+// API Call
+import { getProducts } from "./helper/coreapicalls";
+
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+
+  const loadAllProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
   return (
     <Base title="Home Page" description="I love Ankita">
       <div className="row text-center">
-        <div className="col-4">
-          <Card />
-        </div>
-        <div className="col-4">
-          <Card />
-        </div>
-        <div className="col-4">
-          <Card />
-        </div>
+        <p className="display-5 text-white">All T-Shirts</p>
+        {products.map((product, index) => (
+          <div key={index} className="col-4 mb-4">
+            {" "}
+            <Card />
+          </div>
+        ))}
       </div>
     </Base>
   );
